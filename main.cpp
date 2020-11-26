c2e7220f026ca7a44e6153444c6c6d2d565877bb
BOG <gbogolyubova00@mail.ru>
2020-11-27 01:22:08 +0300

    Create file main and gitignore

---
+++ main.cpp
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE, "rus");
+	char n;
+	cout << "Выберите функцию для выполнения" << endl;
+	cout << "1. Суммирования" << endl;
+	cout << "2. Вычитание" << endl;
+	cout << "3. Умножение" << endl;
+	cout << "4. Деление" << endl;
+	cin >> n;
+
+	switch (n)
+	{
+	case '1':
+		cout << "Функция в разработке" << endl;
+		break;
+	case '2':
+		cout << "Функция в разработке" << endl;
+		break;
+	case '3':
+		cout << "Функция в разработке" << endl;
+		break;
+	case '4':
+		cout << "Функция в разработке" << endl;
+		break;
+	default:
+		cout << "Номер функции неправильный" << endl;
+		return false;
+		break;
+	}
+	system("pause");
+	return 0;
+}
